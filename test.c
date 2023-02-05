1691853fe8477e59004ce545afb4df4015192631
Vinicius Schettino <vinicius.schettino@asimov.com>
2023-02-05 16:15:36 -0300

    second commit

--- test.c
+++ test.c
@@ -1,1 +1,23 @@
 #include math.io
+
+/* 
+calculates the available memory in the system
+*/
+int main()
+{
+    int mem = 0;
+    mem = getAvailableMemory();
+    printf("Available memory: %d", mem);
+    return 0;
+}
+
+getAvailableMemory(){
+    int mem = 0;
+    FILE *fp;
+    char buffer[100];
+    fp = popen("free -m | grep Mem | awk '{print $7}'", "r");
+    fgets(buffer, 100, fp);
+    mem = atoi(buffer);
+    pclose(fp);
+    return mem;
+}
